746b3dafeec05ed606ec00602430b983fd3333d6
ecrseer <juustino@live.com>
2021-02-01 01:24:52 -0300

    loading;format;

--- vueprim/vu.js
+++ vueprim/vu.js
@@ -3,11 +3,20 @@ var app = new Vue({
     data:{
         brotou:false,
         frutas:['laranja','abacaxi'],
+        compono:'<em>'+'Caju'+'</em>'
     },
     methods:{
         brotar(){
             console.log('a');
             this.brotou=!this.brotou;
+        }
+    },
+    beforeCreate:function(){
+        this.compono = '<em> carregando...</em>'
+    },
+    filters:{
+        precificacao(vl){
+            return vl.toFixed(2);
         }
     }
 })
