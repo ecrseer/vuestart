fbc0f44c382075167b6e1149651ba3c26a991259
ecrseer <juustino@live.com>
2021-02-05 15:11:40 -0300

    sacola de frutas

--- vueprim/vu.js
+++ vueprim/vu.js
@@ -1,14 +1,21 @@
 var app = new Vue({
     el:'#app',
     data:{
-        brotou:false,
-        frutas:[{nome:'laranja',preco:5.252},{nome:'abacaxi',preco:4.244}],
-        compono:'<em>'+'Minhas frutas'+'</em>'
+        brotou:true,
+        frutas:[{nome:'laranja',preco:5.252},
+        {nome:'abacaxi',preco:4.244}],
+        compono:'<em>'+'Minhas frutas'+'</em>',
+        sacola:[]
     },
     methods:{
         brotar(){
             console.log('a');
             this.brotou=!this.brotou;
+        },
+        adicionaCarrinho(httt,prodselecionado){
+          
+          this.sacola.push(prodselecionado);
+          this.frutas = this.frutas.filter(fru=>fru.nome!=prodselecionado)
         }
     },
     beforeCreate:function(){
