4c8aff35f53154ecd8c1b553061b781d122d6df5
ecrseer <juustino@live.com>
2021-02-15 15:40:39 -0300

    bind class dark mode

--- vueprim/vu.js
+++ vueprim/vu.js
@@ -1,22 +1,48 @@
 var app = new Vue({
     el:'#app',
     data:{
+        tipofruta:[{
+            aspera:201,
+            dura:402,
+            macia:301},
+            {as:201,ma:301}
+        ],
+        bckColor:'darkslategrey',
         brotou:true,
-        frutas:[{id:23,nome:'laranja',preco:5.252},
-        {id:25,nome:'abacaxi',preco:4.244}],
+        frutas:[{id:23,nome:'laranja',
+        preco:5.252,qtd:42,star:6},
+        {id:25,nome:'abacaxi',preco:4.244,
+        qtd:32,star:2}],
         compono:'<em>'+'Minhas frutas'+'</em>',
-        sacola:[]
+        sacola:[],
+        mostraSaco:false,
+        sacolado:true,
+        meunome:'nao pode',
+        meunomefa:'igual',
+        darkmode_ap1:false,
+        tipofrutas:['alecrim','dourado'],
+
     },
     methods:{
-        brotar(){
-            console.log('a');
+        brotar(){            
             this.brotou=!this.brotou;
         },
         adicionaCarrinho(httt,idprodselec){
             
           this.sacola.push(this.frutas.find(fru=>fru.id==idprodselec));
-          this.frutas = this.frutas.filter(
-              fru=>fru.id!=idprodselec)
+          
+        },
+        mostraSacola(){
+            this.mostraSaco=!this.mostraSaco;
+        },
+        mContCss(nber,fruta){
+            console.log('motd call'+fruta.star)
+            return fruta.star - nber>=0;
+            return nber>3?false:true;
+        },
+        ActvDarkMod(){
+           this.darkmode_ap1 = !this.darkmode_ap1;
+           
         }
     },
     beforeCreate:function(){
@@ -34,5 +60,8 @@ var app = new Vue({
                     (acumulador,produ)=>acumulador+produ.preco,0);
                     return tota;
             },
+            frutascomputd(){
+                return this.frutas;
+            }
         }
 })
